7d45dcacb90654663eb3771be30f5f732999be9b
Alhussiny <eng.elhoseny.osama@gmail.com>
2018-09-24 18:28:32 +0200

    Review Edit Changes

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ uses a list of movies to show them in a web page and when you click any film pos
 
 ## known Bugs
 if the movie name is longer than 23 characters it will not render nicely
+
+## Instructions
+you can run this by opening **entertainment.py** in **idle** or any text editor that supports **python** and run it.
+you can also change the movies or add new movies of your choice but don't forget to modify the list at the end of the file to mett your changes so the file will run right.
+
+## Requirements
+* install python on your machine.
+* any modern web browser of your choice will work just well.
--- entertainment.py
+++ entertainment.py
@@ -1,19 +1,34 @@
 import media
 import fresh_tomatoes
 
-# define multiple instances of Movie class
-# each instance is a movie
-# 1st param is movie name
-# 2nd param is poster url
-# 3rd param is trailer url
-mazeRunner = media.Movie("Maze Runner:Death Cure","https://upload.wikimedia.org/wikipedia/en/e/eb/MazeRunnerDeathCureFinalPoster.jpeg","https://www.youtube.com/watch?v=S_9OSktlm6s")
-cloverfield = media.Movie("The Cloverfield Paradox","https://upload.wikimedia.org/wikipedia/en/e/e5/Cloverfield_paradox_poster.jpg","https://www.youtube.com/watch?v=8brYvhEg5Aw")
-blackPanther = media.Movie("Black Panther","https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg","https://www.youtube.com/watch?v=dxWvtMOGAhw")
-mute = media.Movie("Mute","https://upload.wikimedia.org/wikipedia/en/8/82/Mute_poster.png","https://www.youtube.com/watch?v=ma8te7ywEio")
-annihilation = media.Movie("Annihilation","https://upload.wikimedia.org/wikipedia/en/f/f6/Annihilation_%28film%29.png","https://www.youtube.com/watch?v=89OP78l9oF0")
-prospect = media.Movie("Prospect","https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Prospect_film_poster.jpg/220px-Prospect_film_poster.jpg","https://www.youtube.com/watch?v=XBwPVTyKDEY")
+mazeRunner = media.Movie("Maze Runner:Death Cure",
+                         "https://upload.wikimedia.org/wikipedia/en/e/" +
+                         "eb/MazeRunnerDeathCureFinalPoster.jpeg",
+                         "https://www.youtube.com/watch?v=S_9OSktlm6s")
+cloverfield = media.Movie("The Cloverfield Paradox",
+                          "https://upload.wikimedia.org/wikipedia/en/e/" +
+                          "e5/Cloverfield_paradox_poster.jpg",
+                          "https://www.youtube.com/watch?v=8brYvhEg5Aw")
+blackPanther = media.Movie("Black Panther",
+                           "https://upload.wikimedia.org/wikipedia/en/0/" +
+                           "0c/Black_Panther_film_poster.jpg",
+                           "https://www.youtube.com/watch?v=dxWvtMOGAhw")
+mute = media.Movie("Mute",
+                   "https://upload.wikimedia.org/wikipedia/en/8/82/" +
+                   "Mute_poster.png",
+                   "https://www.youtube.com/watch?v=ma8te7ywEio")
+annihilation = media.Movie("Annihilation",
+                           "https://upload.wikimedia.org/wikipedia/en/f/f6/" +
+                           "Annihilation_%28film%29.png",
+                           "https://www.youtube.com/watch?v=89OP78l9oF0")
+prospect = media.Movie("Prospect",
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/b/" +
+                       "b0/Prospect_film_poster.jpg/220px-Prospect_" +
+                       "film_poster.jpg",
+                       "https://www.youtube.com/watch?v=XBwPVTyKDEY")
 
 # create list of movies to pass to fresh_tomatoes open_movies_page function
-moviesList = [mazeRunner,cloverfield,blackPanther,annihilation,prospect,mute]
+moviesList = [mazeRunner, cloverfield, blackPanther, annihilation,
+              prospect, mute]
 # call fresh_tomatoes open_movies_page function
 fresh_tomatoes.open_movies_page(moviesList)
